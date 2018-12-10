a9c8e81379e1624edaf9592fe0b513602fb6d7ea
JolliestJames <cloudismydog@gmail.com>
2018-12-09 20:09:47 -0800

    Update schema and changeset, add hash_password/1

--- lib/boo_api/accounts/user.ex
+++ lib/boo_api/accounts/user.ex
@@ -1,20 +1,38 @@
 defmodule BooApi.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  import Comeonin.Bcrypt, only: [hashpwsalt: 1]
 
+  alias BooApi.Accounts.User
 
   schema "users" do
     field :email, :string
     field :password_hash, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
 
     timestamps()
   end
 
   @doc false
-  def changeset(user, attrs) do
+  def changeset(%User{} = user, attrs) do
     user
-    |> cast(attrs, [:email, :password_hash])
-    |> validate_required([:email, :password_hash])
+    |> cast(attrs, [:email, :password, :password_confirmation])
+    |> validate_required([:email, :password, :password_confirmation])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 8)
+    |> validate_confirmation(:password)
     |> unique_constraint(:email)
+    |> hash_password
+  end
+
+  defp hash_password(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}}
+        ->
+          put_change(changeset, :password_hash, hashpwsalt(pass))
+      _ ->
+          changeset
+    end
   end
 end
