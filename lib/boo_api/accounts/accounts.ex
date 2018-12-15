45c77322349aaf8732574b28f24a33f4e17ad5ac
JolliestJames <cloudismydog@gmail.com>
2018-12-15 14:05:55 -0800

    Add sign_in/2, authorize/2, get_by_email/1 and verify_password/2

--- lib/boo_api/accounts/accounts.ex
+++ lib/boo_api/accounts/accounts.ex
@@ -8,6 +8,41 @@ defmodule BooApi.Accounts do
 
   alias BooApi.Accounts.User
 
+  alias BooApi.Guardian
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+
+  def sign_in(email, password) do
+    case authorize(email, password) do
+      {:ok, user} ->
+        Guardian.encode_and_sign(user)
+      _ ->
+        {:error, :unauthorized}
+    end
+  end
+
+  defp authorize(email, password) when is_binary(email) and is_binary(password) do
+    with {:ok, user} <- get_by_email(email),
+    do: verify_password(password, user)
+  end
+
+  defp get_by_email(email) when is_binary(email) do
+    case Repo.get_by(User, email: email) do
+      nil ->
+        dummy_checkpw()
+        {:error, "Login error."}
+      user ->
+        {:ok, user}
+    end
+  end
+
+  defp verify_password(password, %User{} = user) when is_binary(password) do
+    if checkpw(password, user.password_hash) do
+      {:ok, user}
+    else
+      {:error, :invalid_password}
+    end
+  end
+
   @doc """
   Returns the list of users.
 
