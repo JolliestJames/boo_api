d9f23fa62323b34d4bfbb7e59882a417b00b3e72
JolliestJames <cloudismydog@gmail.com>
2018-12-17 23:35:57 -0800

    Fix resource_from_claims/1

--- lib/guardian.ex
+++ lib/guardian.ex
@@ -1,12 +1,14 @@
 defmodule BooApi.Guardian do
   use Guardian, otp_app: :boo_api
-
+  require IEx
   def subject_for_token(user, _claims) do
     subject = to_string(user.id)
     {:ok, subject}
   end
 
   def resource_from_claims(claims) do
-    id = claims["subject"]
+    id = claims["sub"]
+    resource = BooApi.Accounts.get_user!(id)
+    {:ok, resource}
   end
 end
