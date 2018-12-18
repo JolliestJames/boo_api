92a5283d822ba161dd294005ebaa39096d503715
JolliestJames <cloudismydog@gmail.com>
2018-12-17 23:34:45 -0800

    Add routing for delete, update and show user to api

--- lib/boo_api_web/router.ex
+++ lib/boo_api_web/router.ex
@@ -1,8 +1,14 @@
 defmodule BooApiWeb.Router do
   use BooApiWeb, :router
 
+  alias BooApi.Guardian
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :jwt_authenticated do
+    plug Guardian.AuthPipeline
   end
 
   scope "/api/v1", BooApiWeb do
@@ -12,9 +18,17 @@ defmodule BooApiWeb.Router do
     post "/sign_up", UserController, :create
   end
 
+  scope "/api/v1", BooApiWeb do
+    pipe_through [:api, :jwt_authenticated]
+
+    delete "/user/delete", UserController, :delete
+    put "/user/update", UserController, :update
+    get "/user", UserController, :show
+  end
+
   scope "/api", BooApiWeb do
     pipe_through :api
 
-    resources "/users", UserController, except: [:new, :edit]
+    resources "/users", UserController, except: [:new, :edit, :show]
   end
 end
