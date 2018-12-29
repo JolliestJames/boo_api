6a386c6a98d6f9340ded8d80699ec8948e0952bf
JolliestJames <cloudismydog@gmail.com>
2018-12-29 01:58:36 -0800

    Refactor user controller spec, use exmachina and improve readability

--- test/boo_api_web/controllers/user_controller_test.exs
+++ test/boo_api_web/controllers/user_controller_test.exs
@@ -1,33 +1,25 @@
 defmodule BooApiWeb.UserControllerTest do
   use BooApiWeb.ConnCase
 
-  alias BooApi.Accounts
+  import BooApi.Factory
+
   alias BooApi.Accounts.User
   alias BooApi.Guardian
 
-  @create_attrs %{
-    email: "some@email",
-    password: "password",
-    password_confirmation: "password"
-  }
   @update_attrs %{
     email: "some_updated@email",
     password: "password",
     password_confirmation: "password"
   }
+
   @invalid_attrs %{
     email: nil,
     password: nil,
     password_confirmation: nil
   }
 
-  def fixture(:user) do
-    {:ok, user} = Accounts.create_user(@create_attrs)
-    user
-  end
-
   setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+    {:ok, conn: put_req_header(conn, "accept", "application/json"), valid: params_for(:user)}
   end
 
   describe "index" do
@@ -38,8 +30,8 @@ defmodule BooApiWeb.UserControllerTest do
   end
 
   describe "create user" do
-    test "returns a jwt when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.user_path(conn, :create), user: @create_attrs)
+    test "returns a jwt when data is valid", %{conn: conn, valid: valid} do
+      conn = post(conn, Routes.user_path(conn, :create), user: valid)
       assert %{"jwt" => jwt} = json_response(conn, 200)
     end
 
@@ -50,61 +42,59 @@ defmodule BooApiWeb.UserControllerTest do
   end
 
   describe "show user" do
-    setup [:create_user]
+    setup %{conn: conn} do
+      user = insert(:user)
+      %User{id: id, email: email} = user
+      {:ok, id: id, email: email, conn: conn |> put_auth_header(authorize(user))}
+    end
 
-    test "returns the user's email when valid", %{conn: conn, user: %User{id: id} = user} do
-      {:ok, token, _claims} = Guardian.encode_and_sign(user)
+    test "returns the user's email when valid", %{conn: conn, id: id, email: email} do
+      conn = conn |> get(Routes.user_path(conn, :show))
 
-      conn = conn
-        |> put_req_header("authorization", "Bearer #{token}")
-        |> get(Routes.user_path(conn, :show))
-
-      assert %{"id" => ^id, "email" => "some@email"} = json_response(conn, 200)
+      assert %{"id" => ^id, "email" => ^email} = json_response(conn, 200)
     end
   end
 
   describe "update user" do
-    setup [:create_user]
-
-    test "renders user when data is valid", %{conn: conn, user: %User{id: id} = user} do
-      {:ok, token, _claims} = Guardian.encode_and_sign(user)
-
-      conn = conn
-        |> put_req_header("authorization", "Bearer #{token}")
-        |> put(Routes.user_path(conn, :update), user: @update_attrs)
-
-      assert %{"id" => ^id, "email" => "some_updated@email"} = json_response(conn, 200)["data"]
+    setup %{conn: conn} do
+      user = insert(:user)
+      %User{id: id} = user
+      %{email: email} = @update_attrs
+      {:ok, id: id, email: email, conn: conn |> put_auth_header(authorize(user))}
     end
 
-    test "renders errors when data is invalid", %{conn: conn, user: user} do
-      {:ok, token, _claims} = Guardian.encode_and_sign(user)
+    test "renders user when data is valid", %{conn: conn, id: id, email: email} do
+      conn = conn |> put(Routes.user_path(conn, :update), user: @update_attrs)
 
+      assert %{"id" => ^id, "email" => ^email} = json_response(conn, 200)["data"]
+    end
 
-      conn = conn
-        |> put_req_header("authorization", "Bearer #{token}")
-        |> put(Routes.user_path(conn, :update), user: @invalid_attrs)
+    test "renders errors when data is invalid", %{conn: conn} do
+      conn = conn |> put(Routes.user_path(conn, :update), user: @invalid_attrs)
 
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
 
   describe "delete user" do
-    setup [:create_user]
+    setup %{conn: conn} do
+      {:ok, conn: conn |> put_auth_header(authorize(insert(:user)))}
+    end
 
-    test "deletes chosen user", %{conn: conn, user: user} do
-      {:ok, token, _claims} = Guardian.encode_and_sign(user)
-
-      conn = conn
-        |> put_req_header("authorization", "Bearer #{token}")
-        |> delete(Routes.user_path(conn, :delete))
+    test "deletes chosen user", %{conn: conn} do
+      conn = conn |> delete(Routes.user_path(conn, :delete))
 
       assert response(conn, 204)
       assert_error_sent 404, fn -> get(conn, Routes.user_path(conn, :show)) end
     end
   end
 
-  defp create_user(_) do
-    user = fixture(:user)
-    {:ok, user: user}
+  defp authorize(user) do
+    {:ok, token, _claims} = Guardian.encode_and_sign(user)
+    token
+  end
+
+  defp put_auth_header(conn, token) do
+    put_req_header(conn, "authorization", "Bearer #{token}")
   end
 end
