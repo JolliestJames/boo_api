53870c3fb3ac45be611b58e8590c754925400042
JolliestJames <cloudismydog@gmail.com>
2018-12-30 14:30:00 -0800

    Define mock for Bcrypt dependency

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,4 @@
 {:ok, _} = Application.ensure_all_started(:ex_machina)
+Mox.defmock(BooApi.EncryptionApi.MockBcrypt, for: BooApi.EncryptionApi)
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(BooApi.Repo, :manual)
